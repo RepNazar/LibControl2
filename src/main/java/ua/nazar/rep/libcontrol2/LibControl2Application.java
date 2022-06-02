36a3aa95e0db85717f2313f13f6cdd14c1f366fc
skillfool <Repyanski@gmail.com>
2022-06-02 19:59:39 +0300

    Init

---
+++ src/main/java/ua/nazar/rep/libcontrol2/LibControl2Application.java
@@ -0,0 +1,13 @@
+package ua.nazar.rep.libcontrol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibControl2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibControl2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ua/nazar/rep/libcontrol2/LibControl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nazar.rep.libcontrol2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibControl2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
